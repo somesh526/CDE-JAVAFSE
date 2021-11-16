a71289c0eff12751379c909ce2f24959ca2bde3e
someshjampa <60320194+somesh526@users.noreply.github.com>
2021-11-16 23:08:36 +0530

    Create script.js

---
+++ User Interface Design/ice cream flavours/script.js
@@ -0,0 +1,6 @@
+$(function(){
+    $("p:first").css("color","orange");
+    $(".flavours li").css({'background-color':'lightblue','font-size':'120%'});
+    $("p:last").css("font-size","50%");
+    $("#icecreamfloats li").css("background-color","orange");
+})
